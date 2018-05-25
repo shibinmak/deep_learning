de820c124879bec1576ffea7b2bfc05727e420ee
shibin mak <m.shibin96@gmail.com>
2018-05-25 15:06:05 +0530

    LSTM

---
+++ projects/google stock price prediction[LSTM]/google_stck_price_prediction(LSTM).py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 24 12:42:02 2018
+
+@author: shibin mak
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('GOOG(1).csv')
